e82d25ded56a5c7244cbcb1ee158bbc68704b58d
kenekc18 <kenekc18@gmail.com>
2023-11-13 14:54:31 +0100

    changed

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 let mobi_nav = document.querySelector("#mobile-navi");
 let close_nav = document.querySelector(".close");
 let harmburger = document.querySelector(".hamburger");
+let main = document.querySelector("body");
 
 harmburger.addEventListener('click', ()=>{
     close_nav.style.display = "block";
     harmburger.style.display = "none";
     mobi_nav.style.display = "block";
+})
+
+close_nav.addEventListener('click',()=>{
+    mobi_nav.style.display = "none";
+    close_nav.style.display = "none";
+    harmburger.style.display = "block";
+
 })
